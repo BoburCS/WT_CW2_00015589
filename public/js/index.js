fe9906d9924067263b68bbbbfefaa261a48e28e7
Bobby <bobur0218programmer@gmail.com>
2024-03-14 11:23:56 +0500

    some fixes

--- public/js/index.js
+++ public/js/index.js
@@ -28,7 +28,6 @@ function closeMenubar ()
 
 window.addEventListener("DOMContentLoaded", function ()
 {
-    this.alert("This web application was created to fulfill Web Technology module’s requirements and does not represent an actual company or service");
     if (btnAddIngredient) btnAddIngredient.addEventListener("click", addIngredient);
     
     btnOpenMenubar.addEventListener("click", openMenubar);
